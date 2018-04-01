c58bc7ed2d6cdaf18d35b03a047fdefec2b03ac6
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-04-01 19:58:55 +0800

    🔧 Fixing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
-const { builder, runClean } = require('@messageflow/build');
+const builder = require('@messageflow/build').builder({
+  dist: '.',
+  cleanGlobs: ['./*.js', './*.d.ts', '!./gulpfile.js', '!./json.d.ts']
+});
 
-const build = builder({ dist: '.', ignores: ['**/demo*', '**/test*'] });
-
-gulp.task('clean', runClean([ './*.js', './*.d.ts', '!./gulpfile.js', '!./json.d.ts' ]));
-gulp.task('lint', build.lint);
-gulp.task('default', gulp.series(...['clean', build.lint, build.ts, build.copy]));
+gulp.task('lint', builder.lint);
+gulp.task('default', builder.default);
